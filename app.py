bd7948fbcbeb7b5947f6f5f949251214d4088367
hylo <hylo@posteo.de>
2023-04-02 19:00:33 +0200

    feat: toggle short names, finished files, folded dirs

--- app.py
+++ app.py
@@ -2,49 +2,59 @@ from flask import Flask, request, send_file, jsonify, render_template
 import os
 import pandas as pd
 import subprocess
+import re
 
 app = Flask(__name__)
 
-@app.route('/<path:path>')
+
+@app.route("/<path:path>")
 def index(path):
     # Get the path to the directory to be listed
     # dir_path = request.args.get('dir', '.')
     # dir_path = "/data/arr/media/tv"
     dir_path = os.path.abspath(f"/data/{path}")
 
-    df = pd.read_csv("~/.config/mpv/hey.log", parse_dates=['datetime'])
+    df = pd.read_csv("~/.config/mpv/hey.log", parse_dates=["datetime"])
     # Build a tree of files and directories
     tree = build_tree(df, dir_path)
 
-
     # Render the file tree using the template
-    return render_template('index.html', tree=tree, render_node=render_node)
+    return render_template("index.html", tree=tree, render_node=render_node)
 
-@app.route('/file-clicked', methods=['POST'])
+
+@app.route("/file-clicked", methods=["POST"])
 def file_clicked():
     data = request.json
-    file_path = data['filePath']
-    
+    file_path = data["filePath"]
+
     try:
-        subprocess.run(['mpv', file_path], check=True)
-        return jsonify({'success': True})
+        subprocess.run(["mpv", file_path], check=True)
+        return jsonify({"success": True})
     except subprocess.CalledProcessError as e:
-        print('Error:', e)
-        return jsonify({'success': False, 'error': str(e)})
+        print("Error:", e)
+        return jsonify({"success": False, "error": str(e)})
+
 
 def render_node(node):
-    meter = f'''<meter min="0" max="100" low="0" high="50" optimum="90" value="{node["ratio"]}"></meter>'''
-    if node['type'] == 'directory':
-        child_nodes = ''.join([render_node(child) for child in node['children']])
-        ratio = node['ratio']
+    meter = f"""<meter min="0" max="100" low="0" high="50" optimum="90" value="{node["ratio"]}"></meter>"""
+    if node["type"] == "directory":
+        child_nodes = "".join([render_node(child) for child in node["children"]])
+        ratio = node["ratio"]
         # open = "open" if 0 < ratio < 90 else ""
-        return f'<li class="directory" data-path="{node["path"]}"><span>{meter} {node["name"]}</span><ul>{child_nodes}</ul></li>'
+        return f'<li class="directory" data-path="{node["path"]}">{meter}<span class="dirname"> {node["name"]}</span><ul>{child_nodes}</ul></li>'
     else:
-        return f'<li><span class="file" data-path="{node["path"]}">{meter} {node["name"]}</span></li>'
+        short_name = extract_episode(node["name"])
+        return f'<li class="file" data-full-name="{node["name"]}" data-short-name="{short_name}" data-path="{node["path"]}">{meter}<span class="filename" >{node["name"]}</span></li>'
+
 
 def build_tree(df, dir_path):
     # Initialize an empty tree
-    tree = {'name': os.path.basename(dir_path), 'path': dir_path, 'type': 'directory', 'children': []}
+    tree = {
+        "name": os.path.basename(dir_path),
+        "path": dir_path,
+        "type": "directory",
+        "children": [],
+    }
 
     # Recursively add files and directories to the tree
     for filename in os.listdir(dir_path):
@@ -52,17 +62,22 @@ def build_tree(df, dir_path):
         if os.path.isdir(file_path):
             # Add a directory to the tree
             sub_tree = build_tree(df, file_path)
-            tree['children'].append(sub_tree)
+            tree["children"].append(sub_tree)
         else:
             # Add a file to the tree
             ratio = calculate_ratio_for_file(df, filename)
-            file_node = {'name': filename, 'path': file_path, 'type': 'file', 'ratio':ratio}
-            tree['children'].append(file_node)
+            file_node = {
+                "name": filename,
+                "path": file_path,
+                "type": "file",
+                "ratio": ratio,
+            }
+            tree["children"].append(file_node)
 
-    n_children = len(tree['children'])
+    n_children = len(tree["children"])
     if n_children == 0:
         n_children = 1
-    tree['ratio'] = sum([node['ratio'] for node in tree['children']]) / n_children
+    tree["ratio"] = sum([node["ratio"] for node in tree["children"]]) / n_children
 
     return tree
 
@@ -81,17 +96,27 @@ def calculate_ratio_for_file(df, filename):
         int: The ratio of pos to duration rounded to an integer.
     """
 
-    file_df = df.loc[df['filename'] == filename]
+    file_df = df.loc[df["filename"] == filename]
     if file_df.empty:
         return 0
 
     last_row = file_df.iloc[-1]
-    ratio = last_row['pos'] / last_row['duration']
+    ratio = last_row["pos"] / last_row["duration"]
     percent = int(round(ratio * 100))
     if percent > 90:
         percent = 100
     return percent
 
 
-if __name__ == '__main__':
+def extract_episode(string):
+    pattern = r"[sS]\d\d[eE]\d\d"
+    matches = re.findall(pattern, string)
+    if matches:
+        first_match = matches[0]
+        return re.findall(r"[eE]\d\d", first_match)[0]
+    else:
+        return string
+
+
+if __name__ == "__main__":
     app.run(debug=True)
